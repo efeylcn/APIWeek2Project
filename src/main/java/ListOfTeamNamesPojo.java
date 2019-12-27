6e9dcb3d252e8f3be2ff7d6635410466f0fdc2ab
nurzhigitgulaiym <gdzhorobekova@gmail.com>
2019-12-26 21:29:25 -0600

    added gulaiym tests

---
+++ src/main/java/ListOfTeamNamesPojo.java
@@ -0,0 +1,15 @@
+import java.util.List;
+import java.util.Map;
+
+public class ListOfTeamNamesPojo {
+
+private List <TeamNamePojo> teams;
+
+    public List<TeamNamePojo> getTeams() {
+        return teams;
+    }
+
+    public void setTeams(List<TeamNamePojo> teams) {
+        this.teams = teams;
+    }
+}
